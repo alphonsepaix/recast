74278955d5a19934b0f8ebbe6dd5099d4a7031bb
Alphonse Paix <alphonse.paix@outlook.com>
2023-08-04 15:32:42 +0200

    Formatage selon PEP 8

--- etas.py
+++ etas.py
@@ -1,31 +1,36 @@
-import numpy as np
-import pandas as pd
+"""Ce fichier fournit une implémentation de l'algorithme ETAS."""
+
+
 from collections.abc import Iterable
 from typing import Optional
+
+import numpy as np
+import pandas as pd
+
 from distributions import ETAS
 
 
-def etas(mu: float = 1,
-         alpha: float = 2,
-         bar_n:float = 0.9,
-         p: float = 1.1, 
-         c: float = 1e-9, 
-         beta: float = np.log(10), 
-         t_end: float = 1e3,
-         max_len: Optional[int] = None
-) -> np.ndarray | None:
-    """Generate an ETAS sequence for the given parameters."""
+def logrand():
+    return np.log(np.random.uniform())
 
-    def logrand(): return np.log(np.random.uniform())
 
+def etas(mu: float = 1, alpha: float = 2, bar_n: float = 0.9, p: float = 1.1, 
+         c: float = 1e-9, beta: float = np.log(10), t_end: float = 1e3,
+         max_len: Optional[int] = None) -> np.ndarray | None:
+    """Génère et renvoie une séquence ETAS dans un tableau NumPy
+    (le premier axe contient les temps d'arrivée, le deuxième les
+    magnitudes et le troisième les parents correspondants).
+    """
+    if max_len is not None and max_len < 0:
+        raise ValueError('max_len must be positive')
+    
     A = bar_n / (beta * c ** (1 - p) / ((p - 1) * (beta - alpha)))
     tc = 0
-
     t = np.array([])
     m = np.array([])
     parent = np.array([])
 
-    # Generate the background events.
+    # Génère les séismes de fond.
     while tc < t_end:
         dt = -1 / mu * logrand()
         tc += dt
@@ -34,17 +39,21 @@ def etas(mu: float = 1,
             m = np.append(m, -1 / beta * logrand())
             parent = np.append(parent, -1)
 
-    if A <= 0 or len(t) == 0:
-        return
+    if len(t) == 0:
+        return None
     
-    # Generate the aftershocks.
+    if A <= 0:
+        return np.stack([t, m, parent], -1)[:max_len]
+    
     n = 0
     n_aftershocks = 0
+
+    # Génère les répliques.
     while True:
         tc = 0
         while True:
-            tmp = (tc + c) ** (1 - p) + (p - 1) / (A * np.exp(alpha * m[n])) \
-                * logrand()
+            tmp = (tc + c) ** (1 - p) \
+                + (p - 1) / (A * np.exp(alpha * m[n])) * logrand()
             if tmp > 0:
                 dt = tmp ** (1 / (1 - p)) - tc - c
                 tc += dt
@@ -54,9 +63,8 @@ def etas(mu: float = 1,
                     parent = np.append(parent, n)
                     n_aftershocks += 1
 
-                    # Return early if max_len was specified.
-                    # We need to generate at least (max_len - 1) children
-                    # otherwise we will be returning a simple Poisson process.
+                    # On génère au moins max_len - 1 répliques si l'argument
+                    # a été fourni avant de quitter.
                     if max_len and n_aftershocks == max_len - 1:
                         break
                 else:
@@ -64,29 +72,28 @@ def etas(mu: float = 1,
             else:
                 break
                 
-        # Sort each sequence by arrival times.
+        # Trie la séquence par temps d'arrivée.
         idx = np.argsort(t)
         t = t[idx]
         m = m[idx]
         parent = parent[idx]
         n += 1
 
-        # We need to sort before returning.
         if max_len and n_aftershocks == max_len - 1:
             break
 
         if n == len(t):
             break
     
-    # Return a matrix with three dimensions.
     return np.stack([t, m, parent], -1)[:max_len]
 
 
 def create_training_dataset(seqs: list[np.ndarray],
                             t_end: float | list[float]
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Create a dataset suited for model training from a list of sequences."""
-
+    """Génère des listes adaptées pour l'entraînement à partir
+    d'une liste de séquences.
+    """
     seq_lengths = []
     inter_times = []
     magnitudes = []
@@ -94,20 +101,18 @@ def create_training_dataset(seqs: list[np.ndarray],
     if not isinstance(t_end, Iterable):
         t_end = np.repeat(t_end, len(seqs))
 
-    # Get the arrival times, inter-arrival times and magnitudes for each
-    # sequence.
+    # On parcourt chaque séquence.
     for seq, t_end in zip(seqs, t_end):
         seq_lengths.append(len(seq))
         inter_times.append(np.diff(seq[:, 0], prepend=0, append=t_end))
         magnitudes.append(seq[:, 1])
 
-    # Pad the inter-arrival times and magnitudes.
+    # On pad les données.
     max_length = np.max(seq_lengths)
     for i, length in enumerate(seq_lengths):
         inter_times[i] = np.pad(inter_times[i], (0, max_length - length))
         magnitudes[i] = np.pad(magnitudes[i], (0, max_length - length + 1))
 
-    # Cast and return the matrices.
     inter_times = np.array(inter_times)
     magnitudes = np.array(magnitudes)
     seq_lengths = np.array(seq_lengths)
@@ -118,54 +123,57 @@ def generate_dataset(batch_size: int = 10,
                      verbose: bool = False,
                      **kwargs
 ) -> tuple[list[np.ndarray], tuple, float | list[float]]:
-    """Generate a dataset suited for model training."""
-
+    """Génère un jeu de données pour l'entraînement."""
     seqs = []
-    if "max_len" in kwargs:
+    if 'max_len' in kwargs:
         t_end = []
-        # Last event will determine the interval length.
+        # Le dernier événement fixera la fin de l'intervalle.
         kwargs["max_len"] += 1
     else:
-        t_end = kwargs.get("t_end", 1e3)    # t_end default to 1e3.
+        t_end = kwargs.get('t_end', 1e3)
 
+    # On génère chaque séquence. Si max_len est spécifié, on ajoute
+    # le dernier temps d'arrivée de la séquence à la liste t_end
+    # avant de retirer le dernier élément.
     for i in range(batch_size):
         seq = etas(**kwargs)
-        if "max_len" in kwargs:
+        if 'max_len' in kwargs:
             t_end.append(seq[-1, 0])
             seq = np.delete(seq, -1, 0)
         seqs.append(seq)
 
         if verbose:
-            print(f"Generating sequences: {i + 1:4}/{batch_size}\r", end="")
-    print()     # new line
+            print(f'Generating sequences: {i + 1:4}/{batch_size}\r', end='')
+    print()
             
     return seqs, create_training_dataset(seqs, t_end), t_end
 
 
 def to_frame(seq: np.ndarray) -> pd.DataFrame:
-    """Return the sequence as a pandas dataframe."""
-
-    columns = ["time", "magnitude", "parent"]
+    """Convertit la séquence en tableau pandas."""
+    columns = ['time', 'magnitude', 'parent']
     if seq.shape[-1] == 2:
         columns.pop()
     return pd.DataFrame(seq, columns=columns)
 
 
 def log_likelihood(seq: np.ndarray, t_end: float, **kwargs) -> float:
-    """Compute the ETAS log-likelihood on the given sequence."""
-
+    """Calcule la log-vraisemblance de la séquence donnée."""
     t = seq[:, 0]
     m = seq[:, 1]
-    mu = kwargs.get("mu", 1)
+    mu = kwargs.get('mu', 1)
     
+    # La distribution du premier temps d'attente suit une loi exponentielle.
     tau_distributions = [lambda x: mu * np.exp(-mu * x)]
+
     for i in range(1, seq.shape[0]):
         tau_distributions.append(ETAS(t[:i], m[:i], **kwargs).density)
 
     inter_times = np.diff(t, prepend=0)
-
     log_like = 0
-    for f, t in zip(tau_distributions, inter_times):
-        log_like += np.log(f(t))
+    for f, tau in zip(tau_distributions, inter_times):
+        log_like += np.log(f(tau))
+
+    # Il manque le terme de survie.
 
     return log_like / t_end
--- model.py
+++ model.py
@@ -1,34 +1,35 @@
+"""Ce fichier fournit une implémentation TensorFlow du modèle
+RECAST (Oleksandr Shchur).
+"""
+
+
 import pickle
+from typing import Optional
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from typing import Optional
+
 from distributions import *
 
 
 class Model:
-    def __init__(self,
-                 C: int = 32,
-                 dist: Distribution = Weibull,
-                 R: int = 1,
-                 learning_rate: float = 5e-3,
-                 dropout_rate: float = 0.,
-                 beta: float = np.log(10),
-                 strategy: str = "tau_log_tau"
-    ) -> None:
-        """Initialize the model parameters."""
-
-        if strategy not in ["tau_log_tau", "log_tau"]:
-            raise ValueError(f"Unknown strategy for time encoding: {strategy}")
-
-        self.C = C          # Number of units for the GRU layer.
-        self.R = R          # Number of components for the mixture outputted by the dense layer.
+    def __init__(self, C: int = 32, dist: Distribution = Weibull,
+                 R: int = 1, learning_rate: float = 5e-3,
+                 dropout_rate: float = 0., beta: float = np.log(10),
+                 strategy: str = 'tau_log_tau') -> None:
+        """Initialise les paramètres du modèle."""
+        if strategy not in ['tau_log_tau', 'log_tau']:
+            raise ValueError(
+                f'Unknown strategy for time encoding: \'{strategy}\'')
+        
+        self.C = C          # Nombre d'unités dans la couche GRU.
+        self.R = R          # Nombre de composantes dans la loi de mélange.
         self.dist = dist
 
         self.rnn = keras.layers.GRU(
             units=C, return_sequences=True, return_state=True)
         self.dense = keras.layers.Dense(R * (dist.n_params + 1))
-
         self.learning_rate = learning_rate
         self.optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
 
@@ -39,129 +40,109 @@ class Model:
         self.magnitudes = False
         self.eps = 1e-8
         self.tau_mean = 1.
-        self.mag_dist = GutenbergRichter(beta=beta)
+        self.mag_dist = Exponential(beta=beta)
         self.mag_mean = self.mag_dist.mean
           
-
     @property
     def layers(self) -> list[keras.layers.Layer]:
         return [self.rnn, self.dense]
 
-
     @property
     def weights(self) -> list[tf.Tensor]:
         return [layer.get_weights() for layer in self.layers]
-    
 
     @property
     def trainable_weights(self) -> list[tf.Tensor]:
-        """Return the trainable weights for gradient descent."""
-
+        """Renvoie les poids entraînables pour la descente de gradient."""
         return self.rnn.trainable_weights + self.dense.trainable_weights
-    
 
     def encode_times(self, inter_times: np.ndarray) -> tf.Tensor:
-        """Encode the inter-arrival times."""
-        inter_times = tf.expand_dims(tf.cast(inter_times, "float32"), -1)
+        """Encode les temps d'attente."""
+        inter_times = tf.expand_dims(tf.cast(inter_times, 'float32'), -1)
         log_tau = tf.math.log(tf.maximum(inter_times, self.eps))
-        if self.strategy == "log_tau":
+        if self.strategy == 'log_tau':
             return log_tau - tf.math.log(self.tau_mean)
-        elif self.strategy == "tau_log_tau":
+        elif self.strategy == 'tau_log_tau':
             return tf.concat([inter_times - self.tau_mean,
                              log_tau - tf.math.log(self.tau_mean)], -1)
     
-
     def encode_magnitudes(self, magnitudes: np.ndarray) -> tf.Tensor:
-        """Encode the magnitudes."""
-        magnitudes = tf.cast(magnitudes, "float32")
+        """Encode les magnitudes."""
+        magnitudes = tf.cast(magnitudes, 'float32')
         return tf.expand_dims(magnitudes - self.mag_mean, -1)
-    
 
     @tf.function
-    def get_context(self,
-                    inter_times: np.ndarray,
-                    magnitudes: Optional[np.ndarray] = None
-    ) -> tf.Tensor:
+    def get_context(self, inter_times: np.ndarray,
+                    magnitudes: Optional[np.ndarray] = None) -> tf.Tensor:
+        """Cette fonction n'est utilisée que lors de l'entraînement pour
+        calculer le vecteur contexte et effectuer un décalage à droite
+        de manière à ce qu'un élément paramétrise la distribution du suivant.
         """
-        Return the context vector produced by the GRU layer and shifted to the
-        right so that each inter-arrival time parametrizes the distribution
-        of the next one.
-        """
-
         rnn_input = self.encode_times(inter_times)
         if self.magnitudes:
-            rnn_input = tf.concat([rnn_input, self.encode_magnitudes(magnitudes)], -1)
-
+            rnn_input = tf.concat(
+                [rnn_input, self.encode_magnitudes(magnitudes)], -1)
         rnn_output = self.rnn(rnn_input)[0]
-        # Shift the context vector to the right.
+
+        # Effectue un décalage vers la droite.
         context = tf.pad(rnn_output[:, :-1, :], [[0, 0], [1, 0], [0, 0]])
-        
+
         return keras.layers.Dropout(self.dropout_rate)(context, training=True)
 
-
     def get_distributions(self, context: tf.Tensor) -> Mixture:
-        """Return the distributions given the output of the GRU layer."""
-
+        """Retourne la distribution du prochain temps d'attente."""
         dense_output = self.dense(context)
-        # Split the tensors according to the number of components and
-        # the number of parameters of the underlying distribution.
+
+        # On divise le tensor obtenu pour obtenir les poids de la loi
+        # de mélange et les paramètres de chaque composante.
         weights, *params = tf.split(dense_output, 1 + self.dist.n_params, -1)
 
-        # Enforce constraints on the parameters.
+        # On applique les contraintes sur les paramètres.
         weights = tf.nn.softmax(weights)
         params = [tf.nn.softplus(param) for param in params]
 
         return Mixture(weights, self.dist(*params, eps=self.eps))
-    
 
     @tf.function
-    def nll_loss(self,
-                 inter_times: np.ndarray,
-                 seq_lengths: np.ndarray,
-                 magnitudes: Optional[np.ndarray] = None
-    ) -> tf.Tensor:
+    def nll_loss(self, inter_times: np.ndarray, seq_lengths: np.ndarray,
+                 magnitudes: Optional[np.ndarray] = None) -> tf.Tensor:
+        """Retourne la log-vraisemblance (négative) sur les données selon
+        les distributions sorties par le modèle.
         """
-        Return the negative log-likelihood on each sequence of the dataset.
-        """
-
         context = self.get_context(inter_times, magnitudes)
         distributions = self.get_distributions(context)
 
+        # On calcule la log-probabilité et la log-survie pour chaque élément.
         log_prob = distributions.log_prob(inter_times)
         log_surv = distributions.log_survival_function(inter_times)
 
-        # The padded elements are set to 0.
+        # Les termes de log-probabilité paddés sont mis à 0.
         mask = tf.cumsum(tf.ones_like(inter_times), -1) \
-            <= tf.cast(seq_lengths[..., np.newaxis], "float32")
+            <= tf.cast(seq_lengths[..., np.newaxis], 'float32')
         log_prob = tf.where(mask, log_prob, 0)
 
-        # We only select the last inter-arrival time for each sequence
-        # to compute the log-survival term.
+        # On ne sélectionne que le dernier élément de chaque séquence
+        # pour le terme de log-survie.
         last_idx = tf.stack([tf.range(len(seq_lengths)), seq_lengths], -1)
         log_surv = tf.gather_nd(log_surv, last_idx)
-        
+
+        # On calcule la log-vraisemblance et on la renvoie.
         log_like = tf.reduce_sum(log_prob, -1) + log_surv
         return -log_like
 
-
-    def fit(self,
-            inter_times: np.ndarray,
-            seq_lengths: list[int],
-            t_end: float | list[float],
-            epochs: int,
+    def fit(self, inter_times: np.ndarray, seq_lengths: list[int],
+            t_end: float | list[float], epochs: int,
             magnitudes: Optional[np.ndarray] = None,
             return_distributions: bool = False,
-            verbose: int = 1
-    ) -> list[float]:
-        """Train the model by maximum likelihood estimation."""
-
+            verbose: int = 1) -> list[float]:
+        """Entraîne le modèle par maximisation de la vraisemblance."""
         history = {}
         if magnitudes is not None:
             self.magnitudes = True
 
-        inter_times = tf.cast(inter_times, "float32")
-        seq_lengths = tf.cast(seq_lengths, "int32")
-        t_end = tf.cast(t_end, "float32")
+        inter_times = tf.cast(inter_times, 'float32')
+        seq_lengths = tf.cast(seq_lengths, 'int32')
+        t_end = tf.cast(t_end, 'float32')
 
         for epoch in tf.range(1, epochs + 1):
             with tf.GradientTape() as tape:
@@ -170,70 +151,72 @@ class Model:
                 nll_loss = self.nll_loss(inter_times, seq_lengths, magnitudes)
                 loss = tf.reduce_mean(nll_loss / t_end)
 
-                history.setdefault("loss", []).append(loss.numpy())
+                history.setdefault('loss', []).append(loss.numpy())
                 if return_distributions:
-                    history.setdefault("distributions", []).append(distributions)
+                    history.setdefault(
+                        'distributions', []).append(distributions)
 
-            # Perform gradient descent with regard to the trainable
-            # weights of the model.
+            # Effectue la descente de gradient.
             gradients = tape.gradient(loss, self.trainable_weights)
-            self.optimizer.apply_gradients(zip(gradients, self.trainable_weights))
+            self.optimizer.apply_gradients(
+                zip(gradients, self.trainable_weights))
 
             if verbose == 2 \
                 or (verbose == 1 and (epoch == 1 or epoch % 10 == 0)):
-                print(f"Loss at epoch {epoch:>4}: {loss:>8.3f}")
+                print(f'Loss at epoch {epoch:>4}: {loss:>8.3f}')
         
         return history
     
-
     def generate(self,
                  batch_size: int,
                  t_end: float
     ) -> tuple[np.ndarray, np.ndarray]:
-        """Generate a batch of sequences from scratch."""
-
+        """Génère des séquences entières de données."""
+        # Par défaut, le vecteur contexte (state) est nul
+        # et les listes sont vides.
         state = tf.zeros([batch_size, self.C])
         inter_times = tf.zeros([batch_size, 0])
         magnitudes = tf.zeros([batch_size, 0])
         generated = False
 
         while not generated:
+            # On obtient les distributions et les temps d'attente.
             next_distributions = self.get_distributions(state)
-            
             next_inter_times = tf.expand_dims(
                 next_distributions.sample(), -1)
             inter_times = tf.concat([inter_times, next_inter_times], -1)
             rnn_input = self.encode_times(next_inter_times)
 
+            # On tire des magnitudes aléatoirement si nécessaire.
             if self.magnitudes:
                 next_magnitudes = self.mag_dist.sample(size=(batch_size, 1))
                 magnitudes = tf.concat([magnitudes, next_magnitudes], -1)
-                rnn_input = tf.concat([rnn_input, self.encode_magnitudes(next_magnitudes)], -1)
+                rnn_input = tf.concat(
+                    [rnn_input, self.encode_magnitudes(next_magnitudes)], -1)
             
+            # On met à jour le vecteur contexte et on vérifie si la
+            # condition d'arrêt est atteinte.
             state = self.rnn(rnn_input, initial_state=state)[1]
-            
             generated = tf.reduce_min(tf.reduce_sum(inter_times, -1)) >= t_end
 
+        # On retourne les données générées (temps d'arrivée et magnitudes
+        # si besoin) ainsi que les tailles de chaque séquence.
         seq_lengths = np.sum(np.cumsum(inter_times, -1) < t_end, -1)
         generated_data = np.cumsum(inter_times, -1)
         if self.magnitudes:
             generated_data = np.stack([generated_data, magnitudes], -1)
         return generated_data, seq_lengths
             
-
-    def predict(self,
-                arrival_times: np.ndarray,
-                magnitudes: Optional[np.ndarray] = None,
-                n: int = 1,
+    def predict(self, arrival_times: np.ndarray,
+                magnitudes: Optional[np.ndarray] = None, n: int = 1,
                 return_distributions: bool = False
     ) -> tuple[np.ndarray, list[Distribution]] | np.ndarray:
-        """Predict the next arrival times given a past sequence."""
-
+        """Détermine les prochains temps d'attente pour une séquence donnée."""
         inter_times = np.diff(arrival_times, prepend=0)
         rnn_input = self.encode_times(inter_times)
         if self.magnitudes:
-            rnn_input = tf.concat([rnn_input, self.encode_magnitudes(magnitudes)], -1)
-    
+            rnn_input = tf.concat(
+                [rnn_input, self.encode_magnitudes(magnitudes)], -1)
         state = self.rnn(rnn_input[tf.newaxis, ...])[1]
 
         inter_times = []
@@ -250,7 +233,10 @@ class Model:
             if self.magnitudes:
                 next_magnitude = self.mag_dist.sample(size=1) 
                 magnitudes.append(next_magnitude[0])
-                rnn_input = tf.concat([rnn_input[:, tf.newaxis], self.encode_magnitudes(next_magnitude)[..., tf.newaxis]], -1)
+                rnn_input = tf.concat([
+                        rnn_input[:, tf.newaxis],
+                        self.encode_magnitudes(next_magnitude)[..., tf.newaxis]
+                    ], -1)
 
             state = self.rnn(rnn_input, initial_state=state)[1]
 
@@ -261,12 +247,10 @@ class Model:
         if return_distributions:
             return predictions, distributions
         return predictions
-    
 
     @property
     def build_params(self) -> dict:
-        """Return all the build parameters of the model."""
-
+        """Retourne les paramètres du modèle."""
         return {
             "C": self.C,
             "dist": self.dist,
@@ -277,10 +261,8 @@ class Model:
             "strategy": self.strategy
         }
     
-
     def save(self, filename: str) -> None:
-        """Save the model build parameters and weights to a file."""
-
+        """Sauvegarde les paramètres et les poids du modèle dans un fichier."""
         build_params = self.build_params
         rnn_input_dim = self.rnn.weights[0].shape[0]
         with open(filename, "wb") as f:
@@ -289,17 +271,15 @@ class Model:
                          self.magnitudes,
                          rnn_input_dim], f)
 
-
     @staticmethod
     def load(filename: str) -> "Model":
-        """Load a model that was saved with the static method from a file."""
-
+        """Reconstruit un modèle à partir d'un fichier de sauvegarde."""
         with open(filename, "rb") as f:
             build_params, weights, magnitudes, input_dim = pickle.load(f)
 
         model = Model(*build_params.values())
 
-        # Necessary to set the layers weights.
+        # Étape nécessaire pour pouvoir affecter les poids à chaque couche.
         model.rnn(np.random.randn(1, 1, input_dim))
         model.dense(np.random.randn(1, 1, model.C))
         model.magnitudes = magnitudes
